30b551e3a83f3cd5e56918178065522d8ec20b52
iketlenglesedi6-cmd <iketlenglesedi6@gmail.com>
2026-03-12 01:01:09 +0200

    week 2 learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Data Analyst";
+        job3._company = "Netflix";
+        job3._startYear = 2017;
+        job3._endYear = 2019;
+
+        Job job4 = new Job();
+        job4._jobTitle = "Product Designer";
+        job4._company = "Adobe";
+        job4._startYear = 2015;
+        job4._endYear = 2017;
+
+        Job job5 = new Job();
+        job5._jobTitle = "Systems Engineer";
+        job5._company = "Boeing";
+        job5._startYear = 2013;
+        job5._endYear = 2015;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+        myResume._jobs.Add(job4);
+        myResume._jobs.Add(job5);
+
+        myResume.Display();
     }
 }
